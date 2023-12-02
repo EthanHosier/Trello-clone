1b0b5d0837364b5de5e185a41672c97b2e08494f
Your Name <ethanjhosier@gmail.com>
2023-12-02 22:02:41 +0000

    added server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,21 @@
-const OrganizationIdPage = () => {
+import { db } from "@/lib/db";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { Board } from "./board";
+import { Form } from "./form";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
   return (
     <div>
-      Organization Page
+      <Form/>
+
+      <div className="space-y-2">
+        {boards.map(board => (
+          <Board key={board.id} id={board.id} title={board.title}/>
+        ))}
+      </div>
     </div>
   )
 }
