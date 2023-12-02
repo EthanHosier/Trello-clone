3f10cc258dff105c0d25e9f394e99054d5903608
Your Name <ethanjhosier@gmail.com>
2023-12-02 16:09:40 +0000

    added authentication + organizations with clerk

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) { //if on home page
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) { //not authenticated and not on public route
+      return redirectToSignIn({ returnBackUrl: req.url});
+    }
+
+    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/selectOrg") { //if user trying to do anything but don't have organization, force them to create / select one (unless already on the select page).
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  }
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
