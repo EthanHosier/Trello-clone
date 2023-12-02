93f960b9f33a82ab4c918a5e1ee4a3011f5643b3
Your Name <ethanjhosier@gmail.com>
2023-12-02 18:09:06 +0000

    added sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,21 @@
+"use client";
+//sets the correct organization based on the url
+
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
+
+export const OrgControl = () => {
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if(!setActive) return;
+
+    setActive({
+      organization: params.organizationId as string,
+    });
+  }, [setActive, params.organizationId])
+
+  return null;
+};
