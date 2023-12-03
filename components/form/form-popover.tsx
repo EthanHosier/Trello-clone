dae10a26bd4d1b4daba67f1ac5492d2a27de58ee
Your Name <ethanjhosier@gmail.com>
2023-12-03 09:45:51 +0000

    started on board image selection

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -14,6 +14,7 @@ import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
 import { X } from "lucide-react";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode,
@@ -66,6 +67,10 @@ export const FormPopover = ({
         </PopoverClose>
         <form className="space-y-4" action={onSubmit}>
           <div className="space-y-4">
+            <FormPicker
+              id="image"
+              errors={fieldErrors}
+            />
             <FormInput
               id="title"
               label="Board title"
