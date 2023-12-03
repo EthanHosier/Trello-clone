0c604c4676adc1284ed68614a65d1b6dbd7327f8
Your Name <ethanjhosier@gmail.com>
2023-12-03 19:19:31 +0000

    added ability to update board name

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
+
 import { OrgControl } from "./_components/org-control";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization")
+  }
+}
 
 const OrganizationIdLayout = ({
   children,
@@ -7,7 +18,7 @@ const OrganizationIdLayout = ({
 }) => {
   return (
     <>
-      <OrgControl/>
+      <OrgControl />
       {children}
     </>
   );
